8726a4417a5adfa768e43f42ffb5343d534d19e9
andreresende36 <andreresende36@gmail.com>
2023-09-14 18:58:50 -0300

    feature 9: api/users done

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -20,11 +20,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
 from news_rest.views.categories_view import CategoriesViewSet
+from news_rest.views.users_view import UsersViewSet
 # from news_rest.views.news_view import NewsViewSet
-# from news_rest.views.users_view import UsersViewSet
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoriesViewSet)
+router.register(r'users', UsersViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
